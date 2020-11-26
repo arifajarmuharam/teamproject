f6a4dc714151738f2e1fd9b1a8bb7b70714d1ff7
arifajarmuharam <72353265+arifajarmuharam@users.noreply.github.com>
2020-11-26 21:38:35 +0700

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,35 +1,19 @@
-package com.example.projectmobprog_login;
+package com.example.menuutama;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button log;
-    private EditText user,pass;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        user = (EditText) findViewById(R.id.username);
-        pass = (EditText) findViewById(R.id.password);
-        log = (Button) findViewById(R.id.loginbutt);
-        log.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nextpage();
-            }
-        });
-    }
-    public void nextpage(){
-        Intent intent = new Intent(this,MainActivity2.class);
-        startActivity(intent);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+                setAmbienceEnabled();
     }
 }
